fea80a43f6eb52a0974c269da07c559efa0811df
Yoan Commeau <yoan.commeau@ynov.com>
2024-01-23 11:27:08 +0100

    Ajout assets folder

---
+++ data/struct.go
@@ -0,0 +1,14 @@
+package data
+
+/*
+type ArtStruct struct {
+	Name     string `json:"name"`
+	Id       int    `json:"id"`
+	Img      string `json:"img"`
+	Auteur   string `json:"auteur"`
+	Date     string `json:"date"`
+	Content  string `json:"content"`
+	Category string `json:"category"`
+	Preview  string `json:"preview"`
+}
+*/
