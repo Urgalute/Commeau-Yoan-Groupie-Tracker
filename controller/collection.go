82e1c3713c134b47495d3812c87232a878dbffcd
Yoan Commeau <yoan.commeau@ynov.com>
2024-03-19 08:51:13 +0100

    Changement utilisateur

--- controller/collection.go
+++ controller/collection.go
@@ -32,6 +32,7 @@ func Collection(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(data)
 		InitTemp.Temp.ExecuteTemplate(w, "collection", data)
 	}
+	
 
 	if route == "tank" {
 		TabSpec, listSpecErr := GetAllSpec()
